62df6f43d6cf2b2ccbd0a044e50d72c7ffc2d7af
EricWinzer <Eric@Winzer.org>
2026-05-21 11:41:43 +0200

    fix: navigation to the left

--- script.js
+++ script.js
@@ -24,3 +24,34 @@ function renderContact() {
     contentRef.innerHTML = '';
     contentRef.innerHTML += myContact()
 };
+
+var anis;
+
+window.onload = function () {
+    anis = document.querySelectorAll('.ani');
+    for (var i = 0; i < anis.length; i++) {
+        anis[i].style.MozAnimationPlayState = 'running';
+        anis[i].style.webkitAnimationPlayState = 'running';
+        anis[i].style.AnimationPlayState = 'running';
+    }
+}
+
+function schalte() {
+    for (var i = 0; i < anis.length; i++) {
+        if (anis[i].style.webkitAnimationPlayState === 'running') {
+            anis[i].style.webkitAnimationPlayState = 'paused';
+        } else {
+            anis[i].style.webkitAnimationPlayState = 'running';
+        }
+        if (anis[i].style.MozAnimationPlayState === 'running') {
+            anis[i].style.MozAnimationPlayState = 'paused';
+        } else {
+            anis[i].style.MozAnimationPlayState = 'running';
+        }
+        if (anis[i].style.AnimationPlayState === 'running') {
+            anis[i].style.AnimationPlayState = 'paused';
+        } else {
+            anis[i].style.AnimationPlayState = 'running';
+        }
+    }
+}
