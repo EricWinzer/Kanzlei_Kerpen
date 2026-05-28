0c6905adb3e1981c9f734a7dc47989ccce66fc6d
EricWinzer <Eric@Winzer.org>
2026-05-28 14:22:43 +0200

    fix: DB structure

--- assets/scripts/db.js
+++ assets/scripts/db.js
@@ -1,81 +1,124 @@
-let rainData = [
+let data = [
     {
-        'imgSVG': "./assets/img/effertz.svg",
-        'imgPNG': "./assets/img/effertz.png",
-        'objectAlt': 'Rechtsanwältin Annika Effertz',
-        'activities': [
+        field: 'rain',
+        structure: [
             {
-                'area': 'Arbeitsrecht',
-                'descriptionShort': 'Beratung von Arbeitgebern',
-                'description':
-                    [
-                        'Laufende Betreuung von Mandanten mit vielfältigen arbeitsrechtlichen Anliegen'
-                    ]
+                imgSVG: './assets/img/effertz.svg',
+                imgPNG: './assets/img/effertz.png',
+                objectAlt: 'Rechtsanwältin Annika Effertz',
+                activities: [
+                    {
+                        area: 'Arbeitsrecht',
+                        descriptionShort: 'Beratung von Arbeitgebern',
+                        description: [
+                            {
+                                position: 1,
+                                text: 'Laufende Betreuung von <strong>Mandanten</strong> mit vielfältigen arbeitsrechtlichen Anliegen',
+                            },
+                            {
+                                position: 2,
+                                text: 'Anwaltliche Beratung – schnelle und kompetente Hilfe bei Streitfällen am Arbeitsplatz (Abmahnung, Kündigung, Arbeitszeugnis, <strong>Vertretung vor Gericht</strong>)',
+                            },
+                            {
+                                position: 3,
+                                text: '<strong>Tarifvertragsrecht</strong> (Anwendung von Tarifverträgen / Ausgestaltung bis hin zur unternehmensindividuellen Anwendung)',
+                            },
+                            {
+                                position: 4,
+                                text: '<strong>Betriebsverfassungsrecht</strong> (Umgang mit BR / Wahl des BR / Gestaltung von Betriebsvereinbarungen)',
+                            },
+                        ],
+                    },
+                    {
+                        area: 'Steuerrecht',
+                        descriptionShort: 'Steuerliche Beratung',
+                        description: [
+                            {
+                                position: 1,
+                                text: 'Laufende Betreuung von <strong>Mandanten</strong> in steuerlichen Angelegenheiten',
+                            },
+                            {
+                                position: 2,
+                                text: 'Begleitung von Mandanten bei <strong>Betriebsprüfungen</strong>',
+                            },
+                            {
+                                position: 3,
+                                text: 'Gesetzeskonforme <strong>Entgeltabrechnung</strong> (inkl. Überstunden, Prämien, Dienstwagen, Entgeltumwandlung, Reisekosten und sonstige diverse Zulagen)',
+                            },
+                            {
+                                position: 4,
+                                text: 'Erstellung von <strong>Jahresabschlüssen</strong> einschließlich der Fertigung von Steuererklärungen',
+                            },
+                        ],
+                    },
+                    {
+                        area: 'Vertragsmanagement',
+                        descriptionShort: 'Gestaltung',
+                        description: [
+                            {
+                                position: 1,
+                                text: 'Durchführung von <strong>Vertragsmanagement</strong> von der Beratung bis zur Gestaltung von Arbeitsverträgen – sicher, unkompliziert und gesetzeskonform',
+                            },
+                            {
+                                position: 2,
+                                text: 'Gestaltung der <strong>Löhne & Gehälter</strong> sowie Benefits zur Erhöhung der Mitarbeiterzufriedenheit und somit zur Steigerung der Arbeitsmotivation / Produktivität sowie Mitarbeiterbindung und somit Steigerung der Arbeitgeber-Attraktivität',
+                            },
+                        ],
+                    },
+                    {
+                        area: 'Datenschutzrecht',
+                        descriptionShort: 'Datenschutzbeauftragte für Unternehmen',
+                    },
+                ],
             },
-            {
-                'area': 'Steuerrecht',
-                'description': 'Steuerlicher Beratung'
-            },
-            {
-                'area': 'Vertragsmanagement',
-                'description': 'Gestaltung '
-            },
-            {
-                'area': 'Datenschutzrecht',
-                'description': 'Datenschutzbeauftragte für Unternehmen'
-            },
-
         ],
-    }
-];
-
-let schmidtData = [
+    },
     {
-        'imgSVG': "./assets/img/schmidt.svg",
-        'imgPNG': "./assets/img/schmidt.png",
-        'objectAlt': 'HR-Beratung Schmidt',
-        'activities': [
+        field: 'schmidt',
+        structure: [
             {
-                'area': 'Coaching',
-                'description': 'Team- und Einzelcoaching'
+                imgSVG: './assets/img/schmidt.svg',
+                imgPNG: './assets/img/schmidt.png',
+                objectAlt: 'HR-Beratung Schmidt',
+                activities: [
+                    {
+                        area: 'Coaching',
+                        descriptionShort: 'Team- und Einzelcoaching',
+                    },
+                    {
+                        area: 'Wissenstransfer',
+                        descriptionShort: 'Begleitung von Nachfolgeprozessen',
+                    },
+                    {
+                        area: 'HR-Prozesse und Change Management',
+                        descriptionShort: 'Entwicklung, Aufbau und Optimierung maßgeschneiderter HR-Prozesse',
+                    },
+                ],
             },
-            {
-                'area': 'Wissenstransfer',
-                'description': 'Begleitung von Nachfolgeprozessen'
-            },
-            {
-                'area': 'HR-Prozesse und Change Management',
-                'description': 'Entwicklung, Aufbau und Optimierung maßgeschneiderter HR-Prozesse '
-            },
-
         ],
-    }
-];
-
-let ariesData = [
+    },
     {
-        'imgSVG': "./assets/img/aries.svg",
-        'imgPNG': "./assets/img/aries.png",
-        'objectAlt': 'ARIES Büroservice',
-        'activities': [
+        field: 'aries',
+        structure: [
             {
-                'area': 'Consulting',
-                'description': 'Überprüfung der betrieblichen Prozesse'
+                imgSVG: './assets/img/aries.svg',
+                imgPNG: './assets/img/aries.png',
+                objectAlt: 'ARIES Büroservice',
+                activities: [
+                    {
+                        area: 'Consulting',
+                        descriptionShort: 'Überprüfung der betrieblichen Prozesse',
+                    },
+                    {
+                        area: 'Existenzgründerberatung',
+                        descriptionShort: 'Erstellung von Gründerkonzepten',
+                    },
+                    {
+                        area: 'EDV-Organisation und -Beratung',
+                        descriptionShort: 'Begleitung von Digitalisierungsprozessen / Homepageerstellung und -hosting',
+                    },
+                ],
             },
-            {
-                'area': 'Existenzgründerberatung',
-                'description': 'Erstellung von Gründerkonzepten'
-            },
-            {
-                'area': 'EDV-Organisation und -Beratung',
-                'description': 'Begleitung von Digitalisierungsprozessen / Homepageerstellung und - hosting'
-            },
-            {
-                'area': 'Datenschutzrecht',
-                'description': 'Datenschutzbeauftragte für Unternehmen'
-            },
-
         ],
-    }
+    },
 ];
-
