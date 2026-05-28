f971336ff1daca8add71041457020c5d1f017e36
EricWinzer <Eric@Winzer.org>
2026-05-28 12:40:08 +0200

    feat: integrated data in json

---
+++ assets/scripts/db.js
@@ -0,0 +1,81 @@
+let rainData = [
+    {
+        'imgSVG': "./assets/img/effertz.svg",
+        'imgPNG': "./assets/img/effertz.png",
+        'objectAlt': 'Rechtsanwältin Annika Effertz',
+        'activities': [
+            {
+                'area': 'Arbeitsrecht',
+                'descriptionShort': 'Beratung von Arbeitgebern',
+                'description':
+                    [
+                        'Laufende Betreuung von Mandanten mit vielfältigen arbeitsrechtlichen Anliegen'
+                    ]
+            },
+            {
+                'area': 'Steuerrecht',
+                'description': 'Steuerlicher Beratung'
+            },
+            {
+                'area': 'Vertragsmanagement',
+                'description': 'Gestaltung '
+            },
+            {
+                'area': 'Datenschutzrecht',
+                'description': 'Datenschutzbeauftragte für Unternehmen'
+            },
+
+        ],
+    }
+];
+
+let schmidtData = [
+    {
+        'imgSVG': "./assets/img/schmidt.svg",
+        'imgPNG': "./assets/img/schmidt.png",
+        'objectAlt': 'HR-Beratung Schmidt',
+        'activities': [
+            {
+                'area': 'Coaching',
+                'description': 'Team- und Einzelcoaching'
+            },
+            {
+                'area': 'Wissenstransfer',
+                'description': 'Begleitung von Nachfolgeprozessen'
+            },
+            {
+                'area': 'HR-Prozesse und Change Management',
+                'description': 'Entwicklung, Aufbau und Optimierung maßgeschneiderter HR-Prozesse '
+            },
+
+        ],
+    }
+];
+
+let ariesData = [
+    {
+        'imgSVG': "./assets/img/aries.svg",
+        'imgPNG': "./assets/img/aries.png",
+        'objectAlt': 'ARIES Büroservice',
+        'activities': [
+            {
+                'area': 'Consulting',
+                'description': 'Überprüfung der betrieblichen Prozesse'
+            },
+            {
+                'area': 'Existenzgründerberatung',
+                'description': 'Erstellung von Gründerkonzepten'
+            },
+            {
+                'area': 'EDV-Organisation und -Beratung',
+                'description': 'Begleitung von Digitalisierungsprozessen / Homepageerstellung und - hosting'
+            },
+            {
+                'area': 'Datenschutzrecht',
+                'description': 'Datenschutzbeauftragte für Unternehmen'
+            },
+
+        ],
+    }
+];
+
