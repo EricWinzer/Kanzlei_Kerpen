0efc701c46bc06c3f5ec84a513a9e8aeb240e169
EricWinzer <Eric@Winzer.org>
2026-06-05 12:48:06 +0200

    refactor: extends the db.js

--- assets/scripts/db.js
+++ assets/scripts/db.js
@@ -78,14 +78,58 @@ let data = [
             {
                 area: 'Coaching',
                 descriptionShort: 'Team- und Einzelcoaching',
+                description: [
+                    {
+                        position: 1,
+                        text: 'Durchführung von <strong>Teamcoaching</strong> zur Lösung von Konflikten, Verbesserung der Zusammenarbeit und Steigerung der Teamleistung',
+                    },
+                    {
+                        position: 2,
+                        text: 'Erfassen von <strong>Gruppendynamiken</strong> und Anleitung zur Kanalisierung in konstruktive Bahnen',
+                    },
+                    {
+                        position: 3,
+                        text: '<strong>Führungskräfte-Coaching</strong> zur Lösung von indiviuellen Fragen, z.B. wie will ich als Führungskraft meine Mitarbeitenden führen?',
+                    },
+                    {
+                        position: 4,
+                        text: '<strong>Einzelcoaching</strong> zur Entwicklung der persönlichen Kompetenzen, z.B. Selbstreflexion, Selbstbewusstsein, Selbstwirksamkeit, Resilienz, Stressmanagement, Konfliktlösung, Kommunikation und vieles mehr',
+                    },
+
+                ],
             },
             {
                 area: 'Wissenstransfer',
                 descriptionShort: 'Begleitung von Nachfolgeprozessen',
+                description: [
+                    {
+                        position: 1,
+                        text: 'Im Rahmen der Nachfolgeplanung implementieren wir gemeinsam strukturierte <strong>Wissenstransferprozesse</strong>',
+                    },
+                    {
+                        position: 2,
+                        text: 'Erstellen von <strong>Wissensbäumen</strong> bei Renteneintritt',
+                    },
+                    {
+                        position: 3,
+                        text: 'Erstellen von <strong>Wissenslandkarten</strong> bei anderen Austritten, wie z.B. bei Kündigungen oder Todesfällen',
+                    },
+
+                ],
             },
             {
                 area: 'HR-Prozesse und Change Management',
                 descriptionShort: 'Entwicklung, Aufbau und Optimierung maßgeschneiderter HR-Prozesse',
+                description: [
+                    {
+                        position: 1,
+                        text: 'Wir unterstützen bei der Entwicklung, bei Aufbau und der Optimierung maßgeschneiderter <strong>HR-Prozesse</strong> für nachhaltigen Erfolg',
+                    },
+                    {
+                        position: 2,
+                        text: 'Begleitung von <strong>Veränderungsprozessen</strong> / Akzeptanz von Veränderungen innerhalb der Belegschaft',
+                    },
+                ],
             },
         ],
     },
@@ -98,14 +142,58 @@ let data = [
             {
                 area: 'Consulting',
                 descriptionShort: 'Überprüfung der betrieblichen Prozesse',
+                description: [
+                    {
+                        position: 1,
+                        text: 'Wir unterstützen bei der Erfassung von <strong>betrieblichen Prozessen</strong> und zeigen Optimierungspotentiale auf',
+                    },
+                    {
+                        position: 2,
+                        text: 'Begleitung bei der Umsetzung von <strong>Oprimierungssprozessen</strong>',
+                    },
+                ],
             },
             {
                 area: 'Existenzgründerberatung',
                 descriptionShort: 'Erstellung von Gründerkonzepten',
+                description: [
+                    {
+                        position: 1,
+                        text: 'Wir unterstützen bei der Erstellung eines <strong>Gründerkonzeptes</strong> einschließlich Ausarbeitung von Businessplänen, Finanzierungsplänen, Marketingstrategien sowie Begleitung des Finanzierungsprozesses',
+                    },
+                    {
+                        position: 2,
+                        text: 'Begleitung bei der Umsetzung des <strong>Gründerkonzeptes</strong>',
+                    },
+                ],
+
             },
             {
                 area: 'EDV-Organisation und -Beratung',
-                descriptionShort: 'Begleitung von Digitalisierungsprozessen / Homepageerstellung und -hosting',
+                descriptionShort: 'Begleitung von Digitalisierungsprozessen • Homepageerstellung und -hosting',
+                description: [
+                    {
+                        position: 1,
+                        text: 'Erfassung des Ist-Zustandes und des gewünschten Soll-Zustands, Ausarbeitung eines Fahrplans zur Erreichung des gewünschten <strong>Digitalisierungszustands</strong>',
+                    },
+                    {
+                        position: 2,
+                        text: 'Begleitung bei der Umsetzung des <strong>Digitalisierungskonzepts</strong>',
+                    },
+                    {
+                        position: 3,
+                        text: 'Programmierung von <strong>Digitalisierungslösungen</strong>',
+                    },
+                    {
+                        position: 4,
+                        text: 'Erstellung und Pflege einer <strong>Homepage</strong>',
+                    },
+                    {
+                        position: 5,
+                        text: 'Bereitstellung einer <strong>Cloud-Lösung</strong> mit Serverstandort in Kerpen',
+                    },
+
+                ],
             },
         ],
     },
